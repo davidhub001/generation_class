771d620cb6a36db90948191b0a30f98da30d708a
david.propitech <david.propitech@gmail.com>
2021-04-15 16:38:27 +0300

    test

--- classe.php
+++ classe.php
@@ -1,15 +1,25 @@
 <?php
 $table = (string)readline("Entrer le table ex: nomtable(champ,..,champ) : ");
+$i =" ";
 preg_match_all('/(.*)\((.*)\)/', $table, $reg_table);
 $nom_class = $reg_table[1][0].'.php';
-$class_name = $reg_table[1][0];
+$classe_name = $reg_table[1][0];
 $text = "<?php\n";
-$text .= "class ".ucwords($class_name)."\n";
+$text .= "class ".ucwords($classe_name)."\n";
 $text .= "{\n";
 $champ = explode(',',$reg_table[2][0]);
 foreach($champ as $cles=>$val):
-  $text .=  "\t private $".$val.";\n";
+    $text .= "$i private $".$val.";\n";
+endforeach;
+foreach($champ as $key=>$val):
+    //$text .= '/*geters*/'."\n";
+    $text .= "$i public function get".ucwords($val)."(){\n";
+    $text .= "$i $i return ".'$this->'.$val.";\n";
+    $text .= "$i $i}\n";
+    //$text .= '/*seters */'."\n";
+    $text .= "$i public function set".ucwords($val)."($".$val."){\n";
+    $text .= "$i $i".'$this->'.$val." = $".$val.";\n";
+    $text .= "$i $i}\n";
 endforeach;
 $text .= "}\n?>";
 file_put_contents($nom_class,$text);
-
---
+++ test.php
@@ -0,0 +1,26 @@
+<?php
+class Test
+{
+  private $a;
+  private $b;
+  private $c;
+  public function getA(){
+    return $this->a;
+   }
+  public function setA($a){
+   $this->a = $a;
+   }
+  public function getB(){
+    return $this->b;
+   }
+  public function setB($b){
+   $this->b = $b;
+   }
+  public function getC(){
+    return $this->c;
+   }
+  public function setC($c){
+   $this->c = $c;
+   }
+}
+?>
