a6c3a9360b4854426856e86b8ce3ca8c12d8afb1
david.propitech <david.propitech@gmail.com>
2021-04-16 16:28:04 +0300

    master

--- test.php
+++ test.php
@@ -1,42 +1,42 @@
 <?php
 class Test
 {
-  private $id;
-  private $nom;
-  private $prenom;
+	 private $a;
+	 private $b;
+	 private $c;
 
-   public function __construct(array $data){
-    $this->hydrate($data);
-   }
-  public function hydrate(array $data){
-    foreach($data as $key => $value){ 
-      $methode = 'set'.ucwords($key); 
-      if(method_exists($this,$methode)){ 
-        $this->$methode($value); 
-      }
-    }
-   }
+ 	 public function __construct(array $data){
+	 	 $this->hydrate($data);
+	 }
+	 public function hydrate(array $data){
+	 	 foreach($data as $key => $value){ 
+	 	 	 $methode = 'set'.ucwords($key); 
+	 	 	 if(method_exists($this,$methode)){ 
+	 	 	 	 $this->$methode($value); 
+	 	 	 }
+	 	 }
+	 }
 
-   public function getId(){
-    return $this->id;
-   }
+ 	 public function getA(){
+	 	 return $this->a;
+	 }
 
-   public function setId($id){
-   $this->id = $id;
-   }
-  public function getNom(){
-    return $this->nom;
-   }
+ 	 public function setA($a){
+	 	 $this->a = $a;
+	 }
+	 public function getB(){
+	 	 return $this->b;
+	 }
 
-   public function setNom($nom){
-   $this->nom = $nom;
-   }
-  public function getPrenom(){
-    return $this->prenom;
-   }
+ 	 public function setB($b){
+	 	 $this->b = $b;
+	 }
+	 public function getC(){
+	 	 return $this->c;
+	 }
 
-   public function setPrenom($prenom){
-   $this->prenom = $prenom;
-   }
+ 	 public function setC($c){
+	 	 $this->c = $c;
+	 }
 }
 ?>
